e62938630355db48e25c412637097c19679b6a81
mh223ja <mh223ja@student.lnu.se>
2014-11-12 12:51:03 +0000

    working on sorting arrays

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -7,21 +7,26 @@ var age = 0;
 var names = []; //variables as arrays
 var ages = [];
 var ageTotal = 0;
+var maxAge;
+var minAge;
+var avgAge;
 
 var data = [{name: "John Häggerud", age: 37},{name: "Johan Leitet", age:36},{name: "Mats Look", age: 46}];
 
 for (var i = 0; i <= persArr.length; i++) { //set arrays for name and age
     names[i] = persArr[i].name;
     ages[i] = persArr[i].age; 
-}
+     }
+     
+    ages[i].sort();
 
-var maxAge = Math.max(ages[i]);
+  maxAge = persArr[persArr.slice (-1)];
 
-var minAge = Math.min(ages[i]);
+  minAge = ages[0];
 
-ageTotal += ages[i]; //calculate total of ages
+  ageTotal += ages[i]; //calculate total of ages
 
-var avgAge = ageTotal / ages.length;
+  avgAge = ageTotal / ages.length;
 	// Din kod här...
 	
 }
