be218833ddb73503acb59f90ee2a6c38dcab4f75
mh223ja <mh223ja@student.lnu.se>
2014-11-12 11:54:39 +0000

    Creating array 1.4

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -3,18 +3,26 @@
 var makePerson = function(persArr){
 
 var name = "";
-var age;
-var names;
-var ages;
+var age = 0;
+var names = []; //variables as arrays
+var ages = [];
+var ageTotal = 0;
 
 var data = [{name: "John Häggerud", age: 37},{name: "Johan Leitet", age:36},{name: "Mats Look", age: 46}];
 
-for (var i = 0; i <= persArr.length; i++) {
+for (var i = 0; i <= persArr.length; i++) { //set arrays for name and age
     names[i] = persArr[i].name;
-    ages[i] = persArr[i].age;
+    ages[i] = persArr[i].age; 
 }
+
+var maxAge = Math.max(ages[i]);
+
+var minAge = Math.min(ages[i]);
+
+ageTotal += ages[i]; //calculate total of ages
+
+var avgAge = ageTotal / ages.length;
 	// Din kod här...
 	
-
 }
 
