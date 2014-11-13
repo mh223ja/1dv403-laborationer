02c4618dae4e62e6afd65c1d8c891a7e92b27bdd
mh223ja <mh223ja@student.lnu.se>
2014-11-13 08:35:35 +0000

    tryingover

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,33 +1,65 @@
 "use strict";
 
-var makePerson = function(persArr){
+var makePerson = function(persArr) {
 
-var name = "";
-var age = 0;
-var names = []; //variables as arrays
-var ages = [];
-var ageTotal = 0;
-var maxAge;
-var minAge;
-var avgAge;
+    var name ="";
+    var age = 0;
+    var nameArray = []; //variables as arrays
+    var ages = [];
+    var ageTotal = 0;
+    var maxAge;
+    var minAge;
+    var averageAge;
+    var nameString ="";
+    var names ="";
+    
+    
 
-var data = [{name: "John Häggerud", age: 37},{name: "Johan Leitet", age:36},{name: "Mats Look", age: 46}];
+    for (var i = 0; i < persArr.length; i++) { 
+    //set arrays for name and age
+        nameArray [i] = persArr[i].name; 
+        ages [i] = persArr[i].age;
+        ageTotal += ages[i];
+      
+    }
+    
+    ages.sort();
 
-for (var i = 0; i <= persArr.length; i++) { //set arrays for name and age
-    names[i] = persArr[i].name;
-    ages[i] = persArr[i].age; 
-     }
-     
-    ages[i].sort();
+    maxAge = ages[ages.length -1 ];
 
-  maxAge = persArr[persArr.slice (-1)];
+    minAge = ages[ 0 ];
 
-  minAge = ages[0];
+    averageAge = Math.round(ageTotal / ages.length);
+   
+   nameArray.sort(function(a,b)
+   {
+       return a.localeCompare(b, 'sv');
+   });
 
-  ageTotal += ages[i]; //calculate total of ages
+    name = nameArray.toString();
+   
 
-  avgAge = ageTotal / ages.length;
-	// Din kod här...
-	
+    names = name.split(",").join(", ");
+
+    return {
+        minAge: minAge,
+        maxAge: maxAge,
+        averageAge: averageAge,
+        names: names
+    }
 }
 
+
+
+var data = [{
+    name: "John Häggerud",
+    age: 37
+}, {
+    name: "Johan Leitet",
+    age: 36
+}, {
+    name: "Mats Loock",
+    age: 46
+}];
+var result = makePerson(data);
+console.log(result);
