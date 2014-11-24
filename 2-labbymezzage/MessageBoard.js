7350d85fac36b3f8287dbdadb2ec156ae58cadcc
mh223ja <mh223ja@student.lnu.se>
2014-11-24 16:15:14 +0000

    moment 3

---
+++ 2-labbymezzage/MessageBoard.js
@@ -0,0 +1,13 @@
+"use strict"
+
+var MessageBoard = {
+    
+    messages: ["No More Stomach Flu", "get out of the house", "two sick kids"],
+
+init: function()
+{
+var mess = new Message(text, new Date()) 
+MessageBoard.messages[2].getText();
+
+}
+};
--- 2-labbymezzage/message.js
+++ 2-labbymezzage/message.js
@@ -4,6 +4,7 @@
 
 function Message (message, date)
 {
+
     this.getText = function (){ //get message
         return message;
     };
@@ -13,27 +14,34 @@ function Message (message, date)
     };
     
     this.getDate = function() { //get date
+      return date;
     };
     
     this.setDate = function(_date){ //set date
+    date =_date;
         
-    }
+    };
+}
     
-    Message.prototype.toString = function() { 
+    Message.prototype.toString = function() 
         { //string representation of object
             return this.getText() +" ("+this.getDate()+")";
-        }
-    };
+        };
+    
     
     Message.prototype.getHTMLText = function(){
-        return this.getText().replace(/[]/g);
-        
+        return this.getText().replace(/[\n\r]/g);
     };
     
     Message.prototype.getDateText = function() {
     };
     
-    }
+
+    
+
+
+
+
 
 
     
