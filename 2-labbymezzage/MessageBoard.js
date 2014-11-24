4fb27818536abc46fd9adf5fa9dcf5ce25b65aab
mh223ja <mh223ja@student.lnu.se>
2014-11-24 20:04:54 +0000

    finished moment 3, starting 4

--- 2-labbymezzage/MessageBoard.js
+++ 2-labbymezzage/MessageBoard.js
@@ -1,13 +1,18 @@
-"use strict"
+"use strict";
 
 var MessageBoard = {
-    
+
     messages: ["No More Stomach Flu", "get out of the house", "two sick kids"],
 
-init: function()
-{
-var mess = new Message(text, new Date()) 
-MessageBoard.messages[2].getText();
+    init:function() {
+        var message = new Message("", new Date());
+        console.log("text", new Date());
+        alert(message);
+        alert(message.getText());
+        message.setText("MyText");
+        alert(message);
+       }
+    };
+ MessageBoard[2].getText();
 
-}
-};
+window.onload = MessageBoard.init;
--- 2-labbymezzage/message.js
+++ 2-labbymezzage/message.js
@@ -1,47 +1,32 @@
-"strict use"
+"strict use";
 
+function Message(message, date) {
 
-
-function Message (message, date)
-{
-
-    this.getText = function (){ //get message
+    this.getText = function() { //get message
         return message;
     };
-    
-    this.setText = function(_text){ //set message
+
+    this.setText = function(_text) { //set message
         message = _text;
     };
-    
+
     this.getDate = function() { //get date
-      return date;
+        return date;
     };
-    
-    this.setDate = function(_date){ //set date
-    date =_date;
-        
+
+    this.setDate = function(_date) { //set date
+        date = _date;
+
     };
 }
-    
-    Message.prototype.toString = function() 
-        { //string representation of object
-            return this.getText() +" ("+this.getDate()+")";
-        };
-    
-    
-    Message.prototype.getHTMLText = function(){
-        return this.getText().replace(/[\n\r]/g);
-    };
-    
-    Message.prototype.getDateText = function() {
-    };
-    
 
-    
+Message.prototype.toString = function() { //string representation of object
+    return this.getText() + " (" + this.getDate() + ")";
+};
 
 
+Message.prototype.getHTMLText = function() {
+    return this.getText().replace(/[\n\r]/g);
+};
 
-
-
-
-    
+Message.prototype.getDateText = function() {};
