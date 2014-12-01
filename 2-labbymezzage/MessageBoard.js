c53ca5ebfa47c92257241e5cde217d3f58b66f17
mh223ja <mh223ja@student.lnu.se>
2014-12-01 11:30:14 +0000

    Finished with checklist

--- 2-labbymezzage/MessageBoard.js
+++ 2-labbymezzage/MessageBoard.js
@@ -6,20 +6,24 @@ var MessageBoard = {
 
     init: function() {
         var newMsg = document.getElementById("button"); //links to button
-        //'listens' for enter click
+        newMsg.onclick = MessageBoard.saveMessages;
+        //'listens' for enter click'
         
-        var listenEnter=document.getElementById("textForm");
-       
-       listenEnter.addEventListener("keypress", function(e){          
-            if (!e) {
+
+        var listenEnter = document.getElementById("textForm");
+
+        listenEnter.onkeypress = function(e) {
+            
+            if(!e) {
                 e = window.event;
-        }
-        
-        if (e.keyCode == 13 && !e.shiftKey){
-            MessageBoard.newMsg(); 
-           }
-        });
-             newMsg.onclick = MessageBoard.saveMessages;
+            }
+
+            if (e.keyCode === 13 && e.shiftKey === false) {
+                MessageBoard.saveMessages();
+                return false;
+            }
+            return true;
+        };
     },
 
     saveMessages: function() {
@@ -34,58 +38,70 @@ var MessageBoard = {
 
 
     renderMessage: function(messageID) {
-        var location=document.getElementById("displayMessage");
+        document.getElementById("textForm").value = ""; //clears message form
+        var location = document.getElementById("displayMessage");
         var newDiv = document.createElement("div"); //adds new div to area
         var messageText = document.createElement("p"); //create paragraph section
+        var messageCount = "Number of Messages is: " + MessageBoard.messages.length;
 
         //create new div
-        newDiv.setAttribute("class","newDiv");
+        newDiv.setAttribute("class", "newDiv");
         location.appendChild(newDiv); //adds new div to DOM
-        
+
         //send message to div
-        messageText.setAttribute("class","messageText");
+        messageText.setAttribute("class", "messageText");
         newDiv.appendChild(messageText);
         messageText.innerHTML = MessageBoard.messages[messageID].getHTMLText();
-        
+
+        var dateDiv = document.createElement("div");
+        var addDate = document.createElement("p");
+
+        dateDiv.setAttribute("class", "newDiv");
+        messageText.appendChild(dateDiv);
+        dateDiv.appendChild(addDate);
+        addDate.innerHTML = MessageBoard.messages[messageID].getDateText();
+
+
+
         //Delete button and delete
-       var deleteButton=document.createElement("img");
-       deleteButton.setAttribute("src","Delete_Icon.png");
-       
-       var deleteMessage=document.createElement("a");
-       deleteMessage.appendChild(deleteButton);
-       
-       location.appendChild(newDiv);
-       newDiv.appendChild(deleteMessage);
-       deleteMessage.appendChild(deleteButton);
-       
-       deleteButton.onclick=function(){
-           if(confirm("Do you really want to erase this message forever?")){
-               MessageBoard.messages.splice(messageID,1);
-               MessageBoard.renderMessages();
-           }
-       };
-       
-       //add clock and time stamp
-       
-       var clock=document.createElement("img");
-       var timePiece=document.createElement("a");
-     
-       
-       clock.setAttribute("src", "clock.png");
-       
-       
-       location.appendChild(newDiv);
-       newDiv.appendChild(timePiece);
-       timePiece.appendChild(clock);
-       
-       clock.onclick=function(){
-         alert("The message was written " + MessageBoard.messages[messageID].getDateText());
-       };
+        var deleteButton = document.createElement("img");
+        deleteButton.setAttribute("src", "Delete_Icon.png");
+
+        var deleteMessage = document.createElement("a");
+        deleteMessage.appendChild(deleteButton);
+
+        location.appendChild(newDiv);
+        newDiv.appendChild(deleteMessage);
+        deleteMessage.appendChild(deleteButton);
+
+        deleteButton.onclick = function() {
+            if (confirm("Do you really want to erase this message forever?")) {
+                MessageBoard.messages.splice(messageID, 1);
+                MessageBoard.renderMessages();
+            }
+        };
+
+        //add clock and time stamp
+
+        var clock = document.createElement("img");
+        var timePiece = document.createElement("a");
+
+
+        clock.setAttribute("src", "clock.png");
+
+
+        location.appendChild(newDiv);
+        newDiv.appendChild(timePiece);
+        timePiece.appendChild(clock);
+
+        clock.onclick = function() {
+            alert("The message was written " + MessageBoard.messages[messageID].getDateText());
+        };
     },
 
     renderMessages: function() {
-        
-        document.getElementById("displayMessage").innerHTML=""; //erases messages so array can write out fresh
+
+        document.getElementById("displayMessage").innerHTML = ""; //erases messages so array can write out fresh
         var i;
 
         for (i = 0; i < MessageBoard.messages.length; ++i) { //renders all messages
--- 2-labbymezzage/message.js
+++ 2-labbymezzage/message.js
@@ -30,12 +30,27 @@ Message.prototype.getHTMLText = function(){
     
 
 Message.prototype.getDateText = function() {
-    var timeOfPost=new Date();
-    var hours = timeOfPost.getHours();
-    var minutes = timeOfPost.getMinutes();
-    var seconds = timeOfPost.getSeconds();
     
-    return hours+":"+minutes+":"+seconds;
+  var todaysDate = this.getDate();
+   var hours = todaysDate.getHours();
+   var minutes = todaysDate.getMinutes();
+   var seconds = todaysDate.getSeconds();
+   
+   
+   if (hours<10) {
+       hours = "0" + hours;
+   }
+   
+   if (minutes < 10){
+       minutes = "0" + minutes;
+   }
+   
+   if (seconds <10) {
+       seconds = "0" + seconds;
+   }
+   return hours + ":" +  minutes + ":" + seconds;
+   
 };
+
 
 
