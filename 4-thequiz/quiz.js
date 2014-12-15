2f7eb6af7c1466b14dffa3af4c418ead204720e9
mh223ja <mh223ja@student.lnu.se>
2014-12-15 10:37:32 +0000

    progress

--- 4-thequiz/quiz.js
+++ 4-thequiz/quiz.js
@@ -2,23 +2,41 @@
 
 var Quiz = {
     init: function(){
+     
+       Quiz.url = "http://vhost3.lnu.se:20080/question/1";
+       Quiz.renderQuiz();
        Quiz.getQuestion();
-       Quiz.url = "http://vhost3.lnu.se:20080/question/1"
+    },
+    
+    renderQuiz: function() {
+        var questionField = document.getElementById("questionField");
+        var newDiv = document.createElement("div");
+        var newP = document.createElement("p");
+        var input = document.createElement("input");
+        var button = document.createElement("button");
+        var answerField = document.getElementById("answerField");
+        
+        questionField.appendChild(newDiv);
+        newDiv.appendChild(newP);
+        
+        
     },
     
     getQuestion: function(){
-        
+        //retriev equestion from server
         var xhr = new XMLHttpRequest ();
         xhr.onreadystatechanged = function(){
             if (xhr.readyState === 4) {
                 if (xhr.status == 200) {
                     console.log(xhr.responseText);
-                    
+                }
+                else {
+                    console.log("read error, status: " +xhr.status);
                 }
             }
             xhr.open("GET", Quiz.url, true);
             xhr.send(null);
-        }
+        };
         
     },
     
