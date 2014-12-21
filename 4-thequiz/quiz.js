c6b3c18364e14118077fd8a3e907a57b62bbb9b7
mh223ja <mh223ja@student.lnu.se>
2014-12-21 21:46:02 +0000

    form clears

--- 4-thequiz/quiz.js
+++ 4-thequiz/quiz.js
@@ -2,47 +2,49 @@
 /*global*/
 
 var Quiz = {
-
+    //start
     //question: undefined,
     //infoURL: "http://vhost3.lnu.se:20080/question/1",
 
-     tries: 0,
+    counter: 0,
+    tries: [],
 
     init: function() {
-
-        Quiz.url = "http://vhost3.lnu.se:20080/question/1";
-        Quiz.getQuestion();
-        Quiz.renderQuiz();
+        //initiates
+        Quiz.url = "http://vhost3.lnu.se:20080/question/1"; //calls first url
+        Quiz.getQuestion(); //runs getQuestion
+        Quiz.renderQuiz(); //runs renderQuiz 
 
 
     },
 
 
-
     renderQuiz: function() {
 
         //sets up form to display quesitons and to give answers
-        var questionField = document.getElementById("questionField");
-        var newDiv = document.createElement("div");
-        var newP = document.createElement("p");
-        var input = document.createElement("input");
-        input.setAttribute("placeholder", "Write your answer here: ");
-        var footer = document.createElement("footer");
-        var triesText = document.createElement("p");
-        triesText.innerHTML = "You have tried to answer this question with " + Quiz.tries;
-        console.log(triesText);
-        var question = document.querySelector("p");
-        var container = document.getElementById("container");
-        
+        var questionField = document.getElementById("questionField"); //gets area
+        var newDiv = document.createElement("div"); //creates div
+        var newP = document.createElement("p"); //creates p element
+        var input = document.getElementById("answerSheet"); //creates input area
+        input.setAttribute("placeholder", "Write your answer here: "); 
+       
+        //sets placeholder
+        var footer = document.createElement("footer"); //creates footer
+        var triesText = document.createElement("p"); //creates tries text area
+        triesText.innerHTML = "You have tried to answer this question with " + Quiz.counter + " tries"; //text tries
+        //console.log(triesText);
+        //var question = document.querySelector("p"); 
+        var container = document.getElementById("container"); //gets entire container
+        console.log(Quiz.tries.length);
 
 
 
-        questionField.appendChild(newDiv);
-        newDiv.appendChild(newP);
+        questionField.appendChild(newDiv); //adds new div to question field
+        newDiv.appendChild(newP); //adds text to new div
 
-        container.appendChild(footer);
-        footer.appendChild(triesText);
-        
+        container.appendChild(footer); //adds footer to container
+        footer.appendChild(triesText); //adds tries text 
+
         //answerField.appendChild(input);
 
         //button.addEventListener("click", Quiz.sendAnswer, console.log("PUSH!"));
@@ -53,23 +55,31 @@ var Quiz = {
     },
 
     getQuestion: function() {
-        Quiz.counter +=1;
-       /* var attempt;
-        console.log(attempt);
-        Quiz.tries.push(attempt);
-        console.log(Quiz.tries.length);*/
+    Quiz.counter++;
+    Quiz.tries.push(Quiz.counter);
+        var footer = document.createElement("footer"); //creates footer
+        var triesText = document.createElement("p"); //creates tries text area
+        footer.appendChild(triesText);
+        console.log(Quiz.counter);
+        console.log(Quiz.tries.length);
+        triesText.innerHTML = "You have tried to answer this question with " + Quiz.counter + " tries"; 
+
+        /* var attempt;
+         console.log(attempt);
+         Quiz.tries.push(attempt);
+         console.log(Quiz.tries.length);*/
         var answerArea = document.getElementById("answerSheet");
-        answerArea.innerHTML="";
-        
+        answerArea.innerHTML = "";
+
         //Quiz.renderQuiz.triesText.innerHTML="You have tried to answer " + Quiz.tries.length + " times";
         //var question = document.querySelector("p");
 
 
         //retriev equestion from server
         console.log("you are here");
-        
+
         var xhr = new XMLHttpRequest();
-     
+
 
 
         xhr.onreadystatechange = function() {
@@ -84,12 +94,7 @@ var Quiz = {
                     console.log(question);
                     var nextUrl = questionRetrieve.nextURL;
                     console.log(nextUrl);
-                    if (question.status === "newQuestion"){
-                        var attempts = Quiz.tries[Quiz.tries.length];
-                        attempts = 1;
-                        var countText = document.getElementById("footer");
-                        countText.innerHTML="You have tried to answer " + attempts-1 + "times";
-                    }
+
                     Quiz.postQuestion(question);
 
 
@@ -103,6 +108,9 @@ var Quiz = {
                 var pushSend = document.getElementById("send");
                 pushSend.onclick = function() {
                     console.log(pushSend);
+                   
+
+
                     Quiz.sendAnswer(nextUrl);
 
                 };
@@ -120,16 +128,29 @@ var Quiz = {
 
     postQuestion: function(question) {
         var postQuestion = document.querySelector("p");
+        var inputClear = document.getElementById("answerSheet");
+        
 
         postQuestion.innerHTML = question;
+        inputClear.innerHTML = "";
+        
 
     },
 
     sendAnswer: function(nextUrl) {
-        
-   
-        var attempts=0;
-        console.log("hi");
+
+        /*Quiz.counter += 1; //adds counter
+        console.log(Quiz.counter);
+        Quiz.tries.push(Quiz.counter);
+        console.log(Quiz.tries);*/
+
+        // var footer = document.getElementById("footer"); //creates footer
+        //var triesText = document.createElement("p"); //creates tries text area
+
+        //footer.appendChild(triesText);
+        //triesText.innerHTML = "";
+        //Quiz.triesText.innerHTML = "You have tried to complete this quiz in " + Quiz.tries.length + " tries";
+        console.log(Quiz.triesText);
         var xhr = new XMLHttpRequest();
         console.log("there");
         xhr.open("POST", nextUrl, true);
@@ -154,6 +175,7 @@ var Quiz = {
                 if (xhr.status === 200) {
                     var question = JSON.parse(xhr.responseText);
                     Quiz.url = question.nextURL;
+                    Quiz.clearText();
                     Quiz.getQuestion(Quiz.url);
                 }
             }
@@ -164,19 +186,26 @@ var Quiz = {
                 console.log("WRONG!");
                 var wrongAnswer = document.createElement("p");
                 var answerSheet = document.getElementById("answerSheet");
+                answerSheet.innerHTML = "";
                 answerSheet.appendChild(wrongAnswer);
                 wrongAnswer.innerHTML = "Wrong! Please try again";
 
             }
 
-        };
+        }
 
         xhr.send(JSON.stringify(finalAnswer));
-       
 
 
+
+    },
+    
+    clearText: function(){
+        var textArea = document.getElementById("answerSheet");
+        textArea.value = '';
+        
     }
-}
+};
 
 //wrongAnswer: function() {
 
